f3120f8d31b8bb990804837686547e6354aae74a
cold-flatwhite <canadasun2022@gmail.com>
2024-07-10 18:05:02 -0700

    added validateName function and onBlur function of TextIput, added a Text when there is a mistake regard inputing wrong name

--- screens/StartScreen.js
+++ screens/StartScreen.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text, TextInput } from "react-native";
 
 const StartScreen = () => {
+  const [name, setName] = useState("");
+  const [nameError, setNameError] = useState(false);
+
+
+  const validateName = (name) => {
+    if (name.length > 1 && !/\d/.test(name)) {
+        setNameError(false);
+    } else {
+        setNameError(true);
+    }
+  }
   return (
     <View>
       <Text>Enter your name:</Text>
-      <TextInput />
+      <TextInput value={name} onChangeText={setName} onBlur={() => validateName(name)}/>
+      {nameError && <Text>Name Error</Text>}
       <Text>Enter your Email:</Text>
       <TextInput />
     </View>
