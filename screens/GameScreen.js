d40a92819b9e74155b2bdff6d305f2ddcb15eac0
cold-flatwhite <canadasun2022@gmail.com>
2024-07-11 22:30:36 -0700

    added Suceess modal in gamescreen

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,17 +1,32 @@
 import React, { useState } from "react";
-import { StyleSheet, View, Text, TextInput, Button } from "react-native";
+import { StyleSheet, View, Text, TextInput, Button, Alert,Modal, Image } from "react-native";
 
 const GameScreen = () => {
-  const [randomNumber, setRandomNumber] = useState(
-    () => Math.floor(Math.random() * 100) + 1
-  );
+  const generateRandomNumber = () => Math.floor(Math.random() * 100) + 1;
+
+  const [randomNumber, setRandomNumber] = useState(generateRandomNumber());
   const [guess, setGuess] = useState("");
   const [attempts, setAttempts] = useState(4);
   const [timer, setTimer] = useState(300);
+  const [showGuessResult, setShowGuessResult] = useState(false);
+  const [showSuccess, setShowSuccess] = useState(false);
+
 
   const handleSubmitGuess = () => {
+    const numGuess = parseInt(guess, 10);
+    if (isNaN(numGuess) || numGuess < 1 || numGuess > 100) {
+      Alert.alert("Invalid Guess", "Please enter a number between 1 and 100.");
+      return;
+    }
     console.log(randomNumber);
-  }
+    if (numGuess === randomNumber) {
+        setShowSuccess(true);
+    } 
+  };
+
+  const handleGameOver = (message) => {
+    Alert.alert("Game Over", message, [{ text: "Restart", onPress: onRestart }]);
+  };
 
   return (
     <View style={styles.gameContainer}>
@@ -27,8 +42,24 @@ const GameScreen = () => {
       <View style={styles.buttonContainer}>
         <Button title="Use a Hint" />
         <Button title="Submit Guess" onPress={handleSubmitGuess} />
-        </View>
+      </View>
+      {showSuccess && (
+        <Modal visible={showSuccess} transparent={true} animationType="slide">
+          <View>
+            <View>
+              <Text>Congratulations! You guessed the number.</Text>
+              <Text>Attempts used: {4 - attempts}</Text>
+              <Image
+                style={styles.image}
+                source={{ uri: `https://picsum.photos/id/${randomNumber}/100/100` }}
+              />
+              <Button title="New Game"/>
+            </View>
+          </View>
+        </Modal>
+      )}
     </View>
+    
   );
 };
 
